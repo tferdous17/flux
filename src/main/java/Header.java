fb155f8ee85871bcbbead9aff51ab34c9f0d44a4
kyoshinoda <kyoshisew@gmail.com>
2024-12-24 14:42:36 -0500

    WIP: Added Header class... missing Headers Class.

--- src/main/java/Header.java
+++ src/main/java/Header.java
@@ -1,3 +1,25 @@
-public class Header {
+public class Header implements HeaderInterface {
+    private final String key;
+    private final byte[] value;
+
+    public Header(String key, byte[] value) {
+        this.key = key;
+        this.value = value;
+    }
+    @Override
+    public String getKey() {
+        return key;
+    }
+    @Override
+    public byte[] getValue() {
+        return value;
+    }
+    @Override
+    public String toString() {
+        return "MyHeader{" +
+                "key='" + key + '\'' +
+                ", value=" + new String(value) +
+                '}';
+    }
 
 }
