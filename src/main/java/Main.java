8832923ef7d0c89521b4dfda611ed250d6db55ce
kyoshinoda <kyoshisew@gmail.com>
2024-12-24 13:24:52 -0500

    WIP: Added methods... attempting testing.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,36 @@
 public class Main {
+//    public producerRecordTest(){
+//
+//    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String key = "Kyoshi";
+        Integer value = 22;
+
+        ProducerRecord<String, Integer> test1 = new ProducerRecord<String, Integer>("Age", 1, Long.valueOf(1412), key, value);
+//        ProducerRecord<String, Integer> test2 = new ProducerRecord<String, Integer>("Age", 1, Long.valueOf(1412), key, value);
+        ProducerRecord<String, Integer> test3 = new ProducerRecord<String, Integer>("Age", 1, key, value);
+//        ProducerRecord<String, Integer> test4 = new ProducerRecord<String, Integer>("Age", 1, Long.valueOf(1412), key, value);
+        ProducerRecord<String, Integer> test5 = new ProducerRecord<String, Integer>("Age", key, value);
+        ProducerRecord<String, Integer> test6 = new ProducerRecord<String, Integer>("Age", value);
+
+
+        System.out.println("TEST 1");
+        System.out.println(test1 + "\n");
+
+//        System.out.println("TEST 2");
+//        System.out.println(test2 + "\n");
+
+        System.out.println("TEST 3");
+        System.out.println(test3 + "\n");
+
+//        System.out.println("TEST 4");
+//        System.out.println(test4 + "\n");
+
+        System.out.println("TEST 5"); // FIX HERE
+        System.out.println(test5 + "\n");
+
+        System.out.println("TEST 6");
+        System.out.println(test6 + "\n");
+
     }
 }
