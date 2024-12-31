d2fd21810eb2d66c0825e6f0b42c1f18262807ab
kyoshinoda <kyoshisew@gmail.com>
2024-12-31 02:19:42 -0500

    Added all testcases with merged stories.

---
+++ src/test/java/producer/FluxProducerTest.java
@@ -0,0 +1,17 @@
+package producer;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FluxProducerTest {
+    @Test
+    public void mapConstructorTest() {
+        Map<String, String> configs = new HashMap<>();
+        configs.put("sample-key", "same-value");
+        FluxProducer<String, String> fluxProducer = new FluxProducer<>(configs);
+
+        System.out.println(fluxProducer);
+    }
+}
