a668ecd05eaaf103a645ce0913c70510cff41e52
kyoshinoda <kyoshisew@gmail.com>
2024-12-31 15:21:31 -0500

    CR Changes: Fixed changes for Generics

--- src/main/java/producer/SerializedProducerRecord.java
+++ src/main/java/producer/SerializedProducerRecord.java
@@ -21,13 +21,15 @@ public class SerializedProducerRecord {
         kryo = new Kryo();
 
         // Kyro requires each class to be registered for serialization. (Includes nested objects)
-        kryo.register(ProducerRecord.class, new ProducerRecordSerializer<>(String.class, Integer.class));
         kryo.register(Optional.class, new OptionalSerializer());
         kryo.register(Headers.class, new HeadersSerializer());
         kryo.register(Header.class, new HeaderSerializer());
     }
 
-    public static <K,V> byte[] serialize(ProducerRecord<K,V> record) {
+    public static <K,V> byte[] serialize(ProducerRecord<K,V> record, Class<K> keyClass, Class<V> valueClass) {
+        // Dynamically register the generic types
+        kryo.register(ProducerRecord.class, new ProducerRecordSerializer<>(keyClass, valueClass));
+
         final Output output = new Output(INITIAL_BUFFER_SIZE, -1); // 4 KB is the typical size of memory page
         kryo.writeObject(output, record);
         output.close();
