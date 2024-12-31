acf247888d96e57d9628f83dbb46986fc4c207d8
tferdous17 <tferdous611@gmail.com>
2024-12-31 14:55:07 -0500

    minor tweaks

--- src/main/java/producer/RecordBatch.java
+++ src/main/java/producer/RecordBatch.java
@@ -7,18 +7,18 @@ import java.util.List;
 
 public class RecordBatch {
     private final int maxBatchSizeInBytes;
-    private int currBatchSize;
-    private int numRecords;
+    private int currBatchSizeInBytes;
     private List<byte[]> batch;
 
+    private static final int DEFAULT_BATCH_SIZE = 10_240; // default batch size: 10 KB = 10,240 bytes
+
     public RecordBatch() {
-        this(10_000); // default batch size: 10 KB = 100,00 bytes
+        this(DEFAULT_BATCH_SIZE);
     }
 
     public RecordBatch(int maxBatchSizeInBytes) {
         this.maxBatchSizeInBytes = maxBatchSizeInBytes;
-        this.currBatchSize = 0;
-        this.numRecords = 0;
+        this.currBatchSizeInBytes = 0;
         this.batch = new ArrayList<>();
     }
 
@@ -26,14 +26,13 @@ public class RecordBatch {
     public boolean append(byte[] serializedRecord) {
         // use a boolean to represent if a record could fit in the current batch or not
         // if not, use the returned `false` value as a signal to create an additional batch
-        if (currBatchSize + serializedRecord.length > maxBatchSizeInBytes) {
-            Logger.warn("Record can not in current batch. New one may be necessary");
+        if (currBatchSizeInBytes + serializedRecord.length > maxBatchSizeInBytes) {
+            Logger.warn("Record can not fit in current batch. New one may be necessary");
             return false;
         }
         // record can fit, so add to batch and return true
         batch.add(serializedRecord);
-        numRecords++;
-        currBatchSize += serializedRecord.length;
+        currBatchSizeInBytes += serializedRecord.length;
 
         return true;
     }
@@ -42,12 +41,12 @@ public class RecordBatch {
         return maxBatchSizeInBytes;
     }
 
-    public int getCurrBatchSize() {
-        return currBatchSize;
+    public int getCurrBatchSizeInBytes() {
+        return currBatchSizeInBytes;
     }
 
-    public int getNumRecords() {
-        return numRecords;
+    public int getRecordCount() {
+        return batch.size();
     }
 
     public List<byte[]> getBatch() {
@@ -55,8 +54,8 @@ public class RecordBatch {
     }
 
     public void printBatchDetails() {
-        System.out.println("Number of Records: " + numRecords);
-        System.out.println("Current Size: " + currBatchSize + " bytes");
+        System.out.println("Number of Records: " + getRecordCount());
+        System.out.println("Current Size: " + currBatchSizeInBytes + " bytes");
         System.out.println("Max Batch Size: " + maxBatchSizeInBytes + " bytes");
     }
 }
