0e032e01c480d0388caa2169c3a7ab5773f4ac29
tferdous17 <tferdous611@gmail.com>
2025-01-02 14:50:01 -0500

    refactor: writeBatchToSegmentTest now verifies content of the file

--- src/test/java/broker/LogSegmentTest.java
+++ src/test/java/broker/LogSegmentTest.java
@@ -3,8 +3,11 @@ package broker;
 import org.junit.jupiter.api.Test;
 import producer.RecordBatch;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 // TODO: Fix failing test cases due to missing file, "./data/partition%d_%05d.log"
 public class LogSegmentTest {
@@ -25,17 +28,20 @@ public class LogSegmentTest {
         LogSegment segment = new LogSegment(0, 0);
         RecordBatch batch = new RecordBatch();
 
-        try {
-            // append fake data
-            batch.append(new byte[]{1, 3, 2, 4, 9, 12, 34, 123, 93});
-            batch.append(new byte[]{45, 4, 85, 5, 9, 12, 34, 123, 93});
-            batch.append(new byte[]{14, 6, 72, 1, 121, 31, 34, 123, 93});
-            batch.append(new byte[]{90, 3, 2, 0, 102, 12, 34, 123, 93});
+        // append fake data
+        batch.append(new byte[]{1, 3, 2, 4, 9, 12, 34, 123, 93});
+        batch.append(new byte[]{45, 4, 85, 5, 9, 12, 34, 123, 93});
+        batch.append(new byte[]{14, 6, 72, 1, 121, 31, 34, 123, 93});
+        batch.append(new byte[]{90, 3, 2, 0, 102, 12, 34, 123, 93});
 
-            // should print "INFO: Batch successfully written to segment."
-            segment.writeBatchToSegment(batch);
-        } catch (IOException e) {
-            e.printStackTrace();
+        // should print "INFO: Batch successfully written to segment."
+        segment.writeBatchToSegment(batch);
+        File file = segment.getLogFile();
+
+        // read the bytes from the file and verify it matches the fake data above
+        byte[] readBytes = Files.readAllBytes(Path.of(file.getPath()));
+        for (byte b : readBytes) {
+            System.out.print(b + " ");
         }
     }
 }
