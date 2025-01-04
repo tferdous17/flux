839732fc615e88c1e3d41672366a03ce06cf80cc
maradC <maradiagachristopher5@gmail.com>
2025-01-04 11:50:05 -0500

    Creating class to check if record can be appended

--- src/main/java/broker/Partition.java
+++ src/main/java/broker/Partition.java
@@ -2,10 +2,33 @@ package broker;
 import producer.RecordBatch;
 import broker.Log;
 
+import java.io.IOException;
+
 
 public class Partition {
 
     private Log log;
     private int currentOffset;
+
+    public Partition(Log log){
+        this.log = log;
+        this.currentOffset = log.getLogEndOffset(); // start from the end of the offset of the log
+    }
+
+    private boolean canAppendRecordToSegment(LogSegment segment, byte[] record) {
+        // Check if the segment's current size plus the record size is within the segment's allowed limit
+        long maxSegmentSize = 10240;
+        long currentSegmentSize = segment.getCurrentSizeInBytes();
+
+        // Check if adding the record would exceed the max segment size
+        return (currentSegmentSize + record.length) <= maxSegmentSize;
+    }
+
+    public void appendRecordBatch(RecordBatch batch) throws IOException {
+        LogSegment logSegment = this.log.getCurrentActiveLogSegment();
+
+        for(byte[] record : batch.getBatch()){
+        }
+    }
 
 }
