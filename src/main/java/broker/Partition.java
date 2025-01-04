4536b2466e92d01c3cb470181d9ba732281b501c
maradC <maradiagachristopher5@gmail.com>
2025-01-04 14:16:25 -0500

    Created the logic for this class and had to use the logsegment to get two varibles need to make a method work

--- src/main/java/broker/Partition.java
+++ src/main/java/broker/Partition.java
@@ -1,34 +1,88 @@
 package broker;
+import org.tinylog.Logger;
 import producer.RecordBatch;
-import broker.Log;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * Extended LogSegment that provides access to private fields we need
+ */
+class ExtendedLogSegment extends LogSegment {
+    public ExtendedLogSegment(int partitionNumber, int startOffset) throws IOException {
+        super(partitionNumber, startOffset);
+    }
+
+    public long currentSizeInBytes;
+    public int endOffset;
+}
 
 public class Partition {
-
     private Log log;
-    private int currentOffset;
+    private int partitionId;
+    private AtomicInteger currentOffset;
+    private static final long MAX_SEGMENT_SIZE = 1_048_576;
 
-    public Partition(Log log){
+    public Partition(Log log, int partitionId) {
         this.log = log;
-        this.currentOffset = log.getLogEndOffset(); // start from the end of the offset of the log
+        this.partitionId = partitionId;
+        this.currentOffset = new AtomicInteger(log.getLogEndOffset());
     }
 
     private boolean canAppendRecordToSegment(LogSegment segment, byte[] record) {
-        // Check if the segment's current size plus the record size is within the segment's allowed limit
-        long maxSegmentSize = 10240;
-        long currentSegmentSize = segment.getCurrentSizeInBytes();
-
-        // Check if adding the record would exceed the max segment size
-        return (currentSegmentSize + record.length) <= maxSegmentSize;
-    }
-
-    public void appendRecordBatch(RecordBatch batch) throws IOException {
-        LogSegment logSegment = this.log.getCurrentActiveLogSegment();
-
-        for(byte[] record : batch.getBatch()){
+        if (!segment.isActive()) {
+            return false;
         }
+        return (segment.getCurrentSizeInBytes() + record.length) <= MAX_SEGMENT_SIZE;
     }
 
+    private LogSegment createNewSegment() throws IOException {
+        ExtendedLogSegment newSegment = new ExtendedLogSegment(partitionId, currentOffset.get());
+        log.getAllLogSegments().add(newSegment);
+
+        Logger.info("Created new log segment starting at offset {}", currentOffset.get());
+        return newSegment;
+    }
+
+    private void appendRecordToSegment(LogSegment segment, byte[] record) {
+        ExtendedLogSegment extSegment = (ExtendedLogSegment) segment;
+        extSegment.currentSizeInBytes += record.length;
+
+        int newOffset = currentOffset.incrementAndGet();
+        extSegment.endOffset = newOffset;
+
+        Logger.debug("Appended record at offset {}", newOffset - 1);
+    }
+
+    public int appendRecordBatch(RecordBatch batch) throws IOException {
+        if (batch == null || batch.getBatch().isEmpty()) {
+            throw new IllegalArgumentException("Cannot append null or empty batch");
+        }
+
+        int batchStartOffset = currentOffset.get();
+        LogSegment activeSegment = log.getCurrentActiveLogSegment();
+
+        for (byte[] record : batch.getBatch()) {
+            if (!canAppendRecordToSegment(activeSegment, record)) {
+                activeSegment = createNewSegment();
+            }
+            appendRecordToSegment(activeSegment, record);
+        }
+
+        Logger.info("Successfully appended {} records starting at offset {}",
+                batch.getRecordCount(), batchStartOffset);
+        return batchStartOffset;
+    }
+
+    public int getCurrentOffset() {
+        return currentOffset.get();
+    }
+
+    public int getPartitionId() {
+        return partitionId;
+    }
+
+    public Log getLog() {
+        return log;
+    }
 }
