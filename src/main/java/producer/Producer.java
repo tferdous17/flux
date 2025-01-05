289bf68e7ead6cc2149dfb88424d3a476cb23ca9
kyoshinoda <kyoshisew@gmail.com>
2025-01-04 21:54:35 -0500

    Added recordAccumulator to FluxProducer

--- src/main/java/producer/Producer.java
+++ src/main/java/producer/Producer.java
@@ -1,6 +1,8 @@
 package producer;
 
+import java.io.IOException;
+
 public interface Producer<K, V> {
-    void send(ProducerRecord<K,V> record);
+    void send(ProducerRecord<K,V> record) throws IOException;
     void close();
 }
