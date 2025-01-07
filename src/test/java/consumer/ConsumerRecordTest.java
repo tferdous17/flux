7c6dd3b51605bb8e7053f66b1986960b905c63cf
Carlos Duque <carlos.d.duque.r@gmail.com>
2025-01-06 19:35:42 -0500

    #11: Address PR comments

--- src/test/java/consumer/ConsumerRecordTest.java
+++ src/test/java/consumer/ConsumerRecordTest.java
@@ -1,37 +1,28 @@
 package consumer;
 
 import commons.header.Header;
+import commons.headers.Headers;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Optional;
 
 public class ConsumerRecordTest {
     String topic = "Test Topic";
     int partition = 1;
     long offset = 1;
     long timestamp = 1704067200;
-    String key = "key";
+    Optional<String> key = Optional.of("key");
     String value = "value";
     Header header1 = new Header("Key1", "Value1".getBytes());
     Header header2 = new Header("Key2", "Value2".getBytes());
-    Iterable<Header> headers = new ArrayList<>(Arrays.asList(header1, header2));
-    ConsumerRecord<String, String> cr = new ConsumerRecord<>(topic, partition, offset, timestamp, key, value, headers);
-
-    @Test
-    public void constructorOneTest() {
-        ConsumerRecord<String, String> test = new ConsumerRecord<>(topic, partition, offset, timestamp);
-        System.out.println(test + "\n");
-    }
-
-    @Test
-    public void constructorTwoTest() {
-        ConsumerRecord<String, String> test = new ConsumerRecord<>(topic, partition, offset, timestamp, key, value, headers);
-        System.out.println(test + "\n");
-    }
+    Headers headers = new Headers();
+    ConsumerRecord<String, String> cr = new ConsumerRecord<String, String>(topic, partition, offset, timestamp, key, value, headers);
 
     @Test
     public void testGetHeaders() {
+        headers.add(header1);
         Iterable<Header> headers = cr.getHeaders();
         for (Header h : headers) {
             h.toString();
@@ -72,11 +63,6 @@ public class ConsumerRecordTest {
     public void testGetTimestamp() {
         long actualTimestamp = cr.getTimestamp();
         System.out.println("Timestamp: " + actualTimestamp);
-    }
-
-    @Test
-    public void testToStringMethod() {
-        System.out.println(cr.toString());
     }
 
 }
