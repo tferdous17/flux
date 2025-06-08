2fcc4e37fb83dde25b1c541b150d288abe9eb562
Tasnim Ferdous <tferdous611@gmail.com>
2025-06-08 15:19:43 -0400

    refactor: abstract log segment functionality out of Partition and into Log

--- src/main/java/broker/Log.java
+++ src/main/java/broker/Log.java
@@ -1,8 +1,14 @@
 package broker;
 
+import org.tinylog.Logger;
+import producer.RecordBatch;
+import proto.Message;
+
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Log represents the entire log for a single partition in Flux, and more specifically is composed of
@@ -10,28 +16,34 @@ import java.util.List;
  * Log, by extension, is essentially a manager for all these segments.
  */
 public class Log {
+    private int partitionId; // id of which partition it belongs to
     private List<LogSegment> segments;
-    private int currentActiveSegmentIdx = 0; // references index in above list
+    private LogSegment activeSegment;
+    private AtomicInteger currentOffset = new AtomicInteger(0);
+
     private int logStartOffset; // record offsets, not bytes
     private int logEndOffset;
-    private int currentSizeInBytes;
 
     // By default, creating a Log will also instantiate 1 empty and mutable LogSegment (default record offset: 0)
-    public Log() throws IOException {
+    public Log(int partitionId) throws IOException {
+        this.partitionId = partitionId;
+
+        LogSegment segment = new LogSegment(partitionId, 0);
         this.segments = new ArrayList<>();
-        try {
-            this.segments.add(new LogSegment(0, 0));
-            this.logStartOffset = segments.getFirst().getStartOffset();
-            this.logEndOffset = segments.getFirst().getEndOffset();
-        } catch (IOException e) {
-            throw e;
-        }
+        this.segments.add(segment);
+        this.activeSegment = segment;
+
+        this.logStartOffset = segments.getFirst().getStartOffset();
+        this.logEndOffset = segments.getFirst().getEndOffset();
     }
 
     // Instantiate log with already-made segment
-    public Log(LogSegment segment) {
+    public Log(LogSegment segment, int partitionId) {
         this.segments = new ArrayList<>();
         this.segments.add(segment);
+        this.activeSegment = segment;
+        this.partitionId = partitionId;
+
         this.logStartOffset = segment.getStartOffset();
         this.logEndOffset = segment.getEndOffset();
     }
@@ -41,19 +53,63 @@ public class Log {
         this.segments = segments;
     }
 
-    // TODO: Implement when RecordBatch is made
-    public void appendRecordBatch() {
-        LogSegment activeSegment = this.getCurrentActiveLogSegment();
+    private boolean canAppendRecordToSegment(byte[] record) {
+        if (activeSegment == null || !activeSegment.isActive()) {
+            return false;
+        }
+        return (activeSegment.getCurrentSizeInBytes() + record.length) <= activeSegment.getSegmentThresholdInBytes();
     }
 
-    // TODO: Implement when ProducerRecord serialization is implemented
-    public void appendSingleRecord() {
-        // always append to the current active segment
-        LogSegment activeSegment = this.getCurrentActiveLogSegment();
+    private LogSegment createNewSegment(int partitionId) throws IOException {
+        LogSegment newSegment = new LogSegment(partitionId, currentOffset.get());
+        segments.add(newSegment);
+        activeSegment = newSegment;
+        System.out.println("CURRENT OFFSET IS: " + currentOffset.get());
+        Logger.info("Created new log segment starting at offset {}", currentOffset.get());
+        return newSegment;
     }
 
-    public LogSegment getCurrentActiveLogSegment() {
-        return this.segments.get(currentActiveSegmentIdx);
+    public int appendRecordBatch(RecordBatch batch) throws IOException {
+        if (batch == null || batch.getRecordCount() == 0 || batch.getCurrBatchSizeInBytes() == 0) {
+            throw new IllegalArgumentException("Batch is empty");
+        }
+        int batchStartOffset = currentOffset.get();
+
+        if (activeSegment == null) {
+            activeSegment = createNewSegment(partitionId);
+        }
+        activeSegment.writeBatchToSegment(batch);
+
+        // Update the offset after writing the batch
+        currentOffset.addAndGet(batch.getRecordCount());
+
+        Logger.info("Successfully appended {} records starting at offset {}", batch.getRecordCount(), batchStartOffset);
+
+        return batchStartOffset;
+    }
+
+    public void appendSingleRecord(byte[] record, int currRecordOffset) throws IOException {
+        if (activeSegment == null) {
+            activeSegment = createNewSegment(partitionId);
+        }
+
+        Logger.info("APPEND SINGLE RECORD: " + Arrays.toString(record));
+        // if writeRecordToSegment returns false, it means the active segment is no longer mutable
+        if (!activeSegment.writeRecordToSegment(record, currRecordOffset)) {
+            // thus we have to create a new active segment to append incoming records to
+            createNewSegment(partitionId).writeRecordToSegment(record, currRecordOffset);
+        };
+        currentOffset.getAndAdd(1);
+        this.logEndOffset = currentOffset.get();
+        Logger.info("2. Successfully appended record to active segment");
+    }
+
+    public Message getRecordAtOffset(int recordOffset) throws IOException {
+        if (activeSegment == null) {
+            System.err.println("Segment is empty. Cannot get record.");
+            return null;
+        }
+        return activeSegment.getRecordFromSegmentAtOffset(recordOffset);
     }
 
     public List<LogSegment> getAllLogSegments() {
@@ -68,17 +124,13 @@ public class Log {
         return logEndOffset;
     }
 
-    public int getCurrentSizeInBytes() {
-        return this.currentSizeInBytes;
-    }
-
     @Override
     public String toString() {
         return "Log{" +
-                ", \ncurrentActiveSegmentIdx=" + currentActiveSegmentIdx +
+                ", \ncurrentActiveSegmentIdx=" +
                 ", \nlogStartOffset=" + logStartOffset +
                 ", \nlogEndOffset=" + logEndOffset +
-                ", \ncurrentSizeInBytes=" + currentSizeInBytes +
+                ", \ncurrentSizeInBytes=" +
                 ", \nsegments=" + segments +
                 '}';
     }
--- src/main/java/broker/Partition.java
+++ src/main/java/broker/Partition.java
@@ -1,13 +1,8 @@
 package broker;
-import org.tinylog.Logger;
 import producer.RecordBatch;
 import proto.Message;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Partition {
@@ -15,75 +10,26 @@ public class Partition {
     private int partitionId;
     private AtomicInteger currentOffset = new AtomicInteger(0);
 
-    private List<LogSegment> segments;
-    private LogSegment activeSegment;
 
     public Partition(int partitionId) throws IOException {
-        // figure out why other log constructor is not creating partition file
         this.partitionId = partitionId;
-        this.segments = new ArrayList<>();
-        LogSegment segment = createNewSegment();
-        this.activeSegment = segment;
-        this.log = new Log(segment);
+        this.log = new Log(partitionId);
         this.currentOffset = new AtomicInteger(log.getLogEndOffset());
     }
 
-    private boolean canAppendRecordToSegment(byte[] record) {
-        if (activeSegment == null || !activeSegment.isActive()) {
-            return false;
-        }
-        return (activeSegment.getCurrentSizeInBytes() + record.length) <= activeSegment.getSegmentThresholdInBytes();
-    }
-
-    private LogSegment createNewSegment() throws IOException {
-        LogSegment newSegment = new LogSegment(partitionId, currentOffset.get());
-        segments.add(newSegment);
-        activeSegment = newSegment;
-        System.out.println("CURRENT OFFSET IS: " + currentOffset.get());
-        Logger.info("Created new log segment starting at offset {}", currentOffset.get());
-        return newSegment;
-    }
-
     public void appendSingleRecord(byte[] record, int currRecordOffset) throws IOException {
-        if (activeSegment == null) {
-            activeSegment = createNewSegment();
+        if (record.length == 0) {
+            return;
         }
-
-        Logger.info("APPEND SINGLE RECORD: " + Arrays.toString(record));
-        // if writeRecordToSegment returns false, it means the active segment is no longer mutable
-        if (!activeSegment.writeRecordToSegment(record, currRecordOffset)) {
-            // thus we have to create a new active segment to append incoming records to
-            createNewSegment().writeRecordToSegment(record, currRecordOffset);
-        };
-        currentOffset.getAndAdd(1);
-        Logger.info("2. Successfully appended record to active segment");
+        this.log.appendSingleRecord(record, currRecordOffset);
     }
 
     public Message getRecordAtOffset(int recordOffset) throws IOException {
-        if (activeSegment == null) {
-            System.err.println("Segment is empty. Cannot get record.");
-            return null;
-        }
-        return activeSegment.getRecordFromSegmentAtOffset(recordOffset);
+        return this.log.getRecordAtOffset(recordOffset);
     }
 
     public int appendRecordBatch(RecordBatch batch) throws IOException {
-        if (batch == null || batch.getRecordCount() == 0 || batch.getCurrBatchSizeInBytes() == 0) {
-            throw new IllegalArgumentException("Batch is empty");
-        }
-        int batchStartOffset = currentOffset.get();
-
-        if (activeSegment == null) {
-            activeSegment = createNewSegment();
-        }
-        activeSegment.writeBatchToSegment(batch);
-
-        // Update the offset after writing the batch
-        currentOffset.addAndGet(batch.getRecordCount());
-
-        Logger.info("Successfully appended {} records starting at offset {}", batch.getRecordCount(), batchStartOffset);
-
-        return batchStartOffset;
+        return this.log.appendRecordBatch(batch);
     }
 
     public int getCurrentOffset() {
@@ -108,9 +54,5 @@ public class Partition {
 
     public void setLog(Log log) {
         this.log = log;
-    }
-
-    public List<LogSegment> getSegments() {
-        return segments;
     }
 }
