eaf6d867221df1b91b87bf143809e284c334292b
Tasnim Ferdous <tferdous611@gmail.com>
2025-07-23 18:32:50 -0400

    change Topic -> proto.Topic for extra clarity + comments

--- src/main/java/admin/NewTopic.java
+++ src/main/java/admin/NewTopic.java
@@ -1,4 +1,6 @@
 package admin;
 
+// Does not represent a formal Topic object, this is just a plain object used by the Admin client when
+// building a createTopics request. Actual topic is validated + constructed by the Controller broker.
 public record NewTopic(String name, int numPartitions, int replicationFactor) {
 }
