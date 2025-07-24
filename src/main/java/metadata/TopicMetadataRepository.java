fdf02d6e6ef4a8109ebb16f7876aecd3d671200b
Tasnim Ferdous <tferdous611@gmail.com>
2025-07-24 16:36:19 -0400

    refactor: define + impl topicExists function in TopicMetadataRepository

--- src/main/java/metadata/TopicMetadataRepository.java
+++ src/main/java/metadata/TopicMetadataRepository.java
@@ -10,7 +10,8 @@ import java.util.Set;
 public interface TopicMetadataRepository {
     void addNewTopic(String topicName, FluxTopic topic);
     boolean deleteTopic(String topicName);
+    boolean topicExists(String topicName);
     Set<String> getActiveTopics();
-    List<Partition> getPartitions(String topicName);
+    List<Partition> getPartitionsFor(String topicName);
     IntRange getPartitionIdRangeForTopic(String topicName);
 }
