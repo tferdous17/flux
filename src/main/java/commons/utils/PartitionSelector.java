e116872d181868b4d4fe7320eb23b68a5f0ed0f7
Tasnim Ferdous <tferdous611@gmail.com>
2025-07-25 17:02:15 -0400

    fix calculation for distributing among a range of partition ids (where min id > 0) + add docs/comments

--- src/main/java/commons/utils/PartitionSelector.java
+++ src/main/java/commons/utils/PartitionSelector.java
@@ -6,6 +6,17 @@ import producer.MurmurHash2;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * Utility class to determine which partition a particular record should go to.
+ * Since multiple fields can help determine partition number, the priority ordering is as follows:
+ *      1. If a partition # is passed in to the record already, this takes top priority.
+ *          1a. Invalid partition numbers automatically get defaulted to round-robin.
+ *      2. If no partition number, attempt key-based hashing.
+ *      3. If no partition number and no key, default to round-robin.
+ *      4. If a topic is also passed in, this just narrows down which partitions we can select for the above operations.
+ *
+ * Note: The print statements are just for verifying correct selection. Should remove them at a later time when no longer necessary.
+ */
 public class PartitionSelector {
     private static AtomicInteger roundRobinCounter = new AtomicInteger(0);
 
@@ -32,6 +43,8 @@ public class PartitionSelector {
             } else {
                 // At this point: Yes topic, no partition #, no key. Default to round-robin within valid range.
                 // Jump to bottom of file for example of how this calculation works.
+                System.out.println("round robin triggering under topic exists, no partition, no key.");
+                System.out.printf("round robin counter = %d minPartition = %d maxPartition = %d range = %d%n", roundRobinCounter.getAndIncrement(), min, max, range);
                 return ((roundRobinCounter.getAndIncrement() - min) % range + range) % range + min;
             }
         } else { // Validate partition num is within topic's range of partitions.
@@ -42,6 +55,8 @@ public class PartitionSelector {
                 // What if it's out of range? Two subcases here:
                 // 1) Fallback to round-robin among this topic's partitions anyway.
                 // or 2) Throw InvalidPartitionException.
+                System.out.println("round robin triggering under topic exists, passed in partition #, no key.");
+                System.out.printf("round robin counter = %d minPartition = %d maxPartition = %d range = %d%n", roundRobinCounter.getAndIncrement(), min, max, range);
                 return ((roundRobinCounter.getAndIncrement() - min) % range + range) % range + min;
             }
         }
@@ -56,6 +71,8 @@ public class PartitionSelector {
             } else {
                 // At this point: No topic, no partition #, no key. Default to round-robin among all partitions in system.
                 // TODO: Should we insert topic-less records into partitions that belong to a particular topic? How to handle? Come back later
+                System.out.println("round robin triggering under NO topic, NO partition, NO key.");
+                System.out.printf("round robin counter = %d, numPartitions = %d", roundRobinCounter.getAndIncrement(), numPartitions);
                 return roundRobinCounter.getAndIncrement() % numPartitions;
             }
         } else { // Validate partition number
@@ -78,16 +95,19 @@ public class PartitionSelector {
         // The & 0x7fffffff operation is a faster way of doing Math.abs()
         // that also handles Integer.MIN_VALUE correctly.
         int positiveHash = hash & 0x7fffffff;
+        System.out.printf("Murmur2 hash returning = %d (no range)".formatted(positiveHash % numPartitions));
         return positiveHash % numPartitions;
     }
 
     public static int selectPartitionWithinRangeUsingMurmurHash2(String key, int min, int max) {
         if (key == null || key.isEmpty()) {
+            System.out.println("Empty/null key, returning = " + min);
             return min; // Instead of 0, our default partition will be the partition w/ the min ID
         }
         int hash = MurmurHash2.hash(key);
         int positiveHash = hash & 0x7fffffff;
-        return min + (positiveHash % max - min + 1);
+        System.out.printf("Murmur2 hash returning = %d within range [%d,%d]%n", min + (positiveHash % (max - min + 1)), min, max);
+        return min + (positiveHash % (max - min + 1));
     }
 }
 
