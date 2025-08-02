447de0cdd8659a46902103aec7e7639361eda44c
Tasnim Ferdous <tferdous611@gmail.com>
2025-08-02 18:18:01 -0400

    refactor: metadata cache is now initially populated within FluxProducer constructor + cache gets auto refreshed on a scheduled interval

--- src/main/java/producer/FluxProducer.java
+++ src/main/java/producer/FluxProducer.java
@@ -1,5 +1,8 @@
 package producer;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.google.protobuf.ByteString;
 import commons.utils.PartitionSelector;
 import io.grpc.Grpc;
@@ -12,24 +15,36 @@ import proto.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.StampedLock;
 
 public class FluxProducer<K, V> implements Producer {
     private final PublishToBrokerGrpc.PublishToBrokerBlockingStub blockingStub;
-    private final MetadataServiceGrpc.MetadataServiceBlockingStub metadataBlockingStub;
+    private final MetadataServiceGrpc.MetadataServiceFutureStub metadataFutureStub;
     private ManagedChannel channel;
-
     List<IntermediaryRecord> buffer;
-    private final ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(1);
+    private final ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(2);
+
+    private FetchBrokerMetadataResponse cachedBrokerMetadata; // read-heavy
+    private final StampedLock stampedLock = new StampedLock();
 
     public FluxProducer(long initialFlushDelay, long flushDelayInterval) {
         channel = Grpc.newChannelBuilder("localhost:50051", InsecureChannelCredentials.create()).build();
         blockingStub = PublishToBrokerGrpc.newBlockingStub(channel);
-        metadataBlockingStub = MetadataServiceGrpc.newBlockingStub(channel);
+        metadataFutureStub = MetadataServiceGrpc.newFutureStub(channel);
         buffer = new ArrayList<>();
+
         scheduledExecutorService.scheduleWithFixedDelay(this::flushBuffer, initialFlushDelay, flushDelayInterval, TimeUnit.SECONDS);
+        scheduledExecutorService.scheduleWithFixedDelay(this::refreshCachedMetadata, 5, 5, TimeUnit.MINUTES);
+
+        // fetch metadata upon initialization and cache the metadata it needs later
+        FetchBrokerMetadataRequest request = FetchBrokerMetadataRequest.newBuilder().build();
+        try {
+            cachedBrokerMetadata = metadataFutureStub.fetchBrokerMetadata(request).get();
+            printMetadataForTesting();
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public FluxProducer(long flushDelayInterval) {
@@ -40,17 +55,67 @@ public class FluxProducer<K, V> implements Producer {
         this(60, 60);
     }
 
+    public void printMetadataForTesting() {
+        System.out.println(cachedBrokerMetadata);
+    }
+
+    /**
+     * To refresh the cache, we will be asynchronously fetching the metadata and upon success we will obtain
+     * an exclusive write lock and replace our cache w/ the updated metadata.
+     */
+    private void refreshCachedMetadata() {
+        Logger.info("REFRESHING CACHED BROKER METADATA");
+        FetchBrokerMetadataRequest request = FetchBrokerMetadataRequest.newBuilder().build();
+        ListenableFuture<FetchBrokerMetadataResponse> future = metadataFutureStub.fetchBrokerMetadata(request);
+        Futures.addCallback(future, new FutureCallback<FetchBrokerMetadataResponse>() {
+            @Override
+            public void onSuccess(FetchBrokerMetadataResponse result) {
+                long stamp = stampedLock.writeLock();
+                try {
+                    cachedBrokerMetadata = result;
+                    Logger.info("SUCCESSFULLY REFRESHED CACHED BROKER METADATA");
+                } finally {
+                    stampedLock.unlockWrite(stamp);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Logger.error(t);
+            }
+        }, Executors.newSingleThreadExecutor());
+    }
+
+    /**
+     *  We are doing optimistic reads since our metadata cache is read-heavy, and optimistic reads
+     *  allow us to perform reads without obtaining a full lock--ideal for scenarios like this.
+     *  NOTE: This assumes we will have minimal contention on our cache (i.e., read-write conflicts are expected to NOT be common)
+     *
+     *  If the `if` block triggers, it means that a conflict has occurred.
+     *  I.e., between the time of us doing the optimistic read and validating, another thread acquired a write lock
+     *  and modified the shared resource. In this case, we will fall back by acquiring an exclusive read lock on the cache.
+     *  Note there exists other fallback mechanisms like repeatedly retrying the read operation or exponential backoff to avoid overwhelming the system w/ retries
+     */
+    private int retrieveCurrentNumBrokerPartitions() {
+        long stamp = stampedLock.tryOptimisticRead();
+        int currentNumBrokerPartitions = cachedBrokerMetadata.getNumPartitions();
+        if (!stampedLock.validate(stamp)) {
+            stamp = stampedLock.readLock();
+            try {
+                currentNumBrokerPartitions = cachedBrokerMetadata.getNumPartitions();
+            } finally {
+                stampedLock.unlockRead(stamp);
+            }
+        }
+        return currentNumBrokerPartitions;
+    }
+
     @Override
     public void send(ProducerRecord record) throws IOException {
-        // need to maintain a buffer of serialized records
-        // then once it reaches a particular threshold, flush it by adding all the recs to a grpc req
-        // and then sending it over to the broker
         Object key = record.getKey() == null ? "" : record.getKey();
         Object value = record.getValue() == null ? "" : record.getValue();
 
-        // fetch our broker's num of partitions to help determine target partition in round-robin scenario
-        FetchBrokerMetadataResponse brokerMetadataResponse = metadataBlockingStub
-                .fetchBrokerMetadata(FetchBrokerMetadataRequest.newBuilder().build());
+        int currentNumBrokerPartitions = retrieveCurrentNumBrokerPartitions();
 
         // Determine target partition, and then serialize.
         int targetPartition = PartitionSelector.getPartitionNumberForRecord(
@@ -58,7 +123,7 @@ public class FluxProducer<K, V> implements Producer {
                 record.getPartitionNumber(),
                 key.toString(),
                 record.getTopic(),
-                brokerMetadataResponse.getNumPartitions() // must use this exact ID for testing
+                currentNumBrokerPartitions
         );
         System.out.println("Record has target partition = " + targetPartition);
         // Serialize data and convert to ByteString (gRPC only takes this form for byte data)
