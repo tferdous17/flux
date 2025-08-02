72b4d2ec2a5be80939399c9f21ecfc16c12c38f0
Tasnim Ferdous <tferdous611@gmail.com>
2025-08-02 18:20:35 -0400

    basic metadata service tests

---
+++ src/test/java/grpc/MetadataServiceTest.java
@@ -0,0 +1,43 @@
+package grpc;
+
+import broker.Broker;
+import org.junit.jupiter.api.Test;
+import producer.FluxProducer;
+
+public class MetadataServiceTest {
+
+    @Test
+    public void testMetadataService() {
+        // start up server first
+        Thread serverThread = new Thread(MetadataServiceTest::startServer);
+        serverThread.start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+    }
+
+    @Test
+    public void testOnlyMetadataCacheAutoRefreshing() {
+        // This will print the initial metadata and then simply refresh (and log) the updated cache.
+        // Change the interval inside FluxProducer--by default its 5 mins (change to 1 min for quicker testing).
+        FluxProducer<String, String> producer = new FluxProducer<>(300, 300);
+        while (true) {}
+    }
+
+    private static void startServer() {
+        try {
+            Broker broker = new Broker();
+            BrokerServer server = new BrokerServer(broker);
+            server.start(50051);
+            server.blockUntilShutdown();
+
+            System.out.println("Server started at port 50051");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
