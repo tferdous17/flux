ac5569d9244287de7283a760e387548394e3fdfa
Tasnim Ferdous <tferdous611@gmail.com>
2025-08-03 18:27:00 -0400

    refactor: create and use static instance of ScheduledExecutor

--- src/main/java/commons/FluxExecutor.java
+++ src/main/java/commons/FluxExecutor.java
@@ -2,9 +2,11 @@ package commons;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class FluxExecutor {
     private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(2 * Runtime.getRuntime().availableProcessors());
+    private static final ScheduledExecutorService SCHEDULER_SERVICE = Executors.newScheduledThreadPool(2);
 
     private FluxExecutor() {} // prevent initialization
 
@@ -12,8 +14,13 @@ public class FluxExecutor {
         return EXECUTOR_SERVICE;
     }
 
+    public static ScheduledExecutorService getSchedulerService() {
+        return SCHEDULER_SERVICE;
+    }
+
     public static void shutdown() {
         EXECUTOR_SERVICE.shutdown();
+        SCHEDULER_SERVICE.shutdown();
     }
 
 }
