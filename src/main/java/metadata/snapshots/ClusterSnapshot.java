1d56b4c7aa2b35383a31a444dc68d092d2bf0560
Tasnim Ferdous <tferdous611@gmail.com>
2025-09-01 17:34:52 -0400

    refactor: broker addr is now the key for each BrokerMetadata entry in ClusterSnapshot

--- src/main/java/metadata/snapshots/ClusterSnapshot.java
+++ src/main/java/metadata/snapshots/ClusterSnapshot.java
@@ -6,7 +6,7 @@ import java.util.Map;
  * Represents an immutable, and fairly comprehensive snapshot of a cluster's entire metadata
  */
 public record ClusterSnapshot(ControllerMetadata controllerMetadata,
-                              Map<String, BrokerMetadata> brokers, // <broker id, metadata>
+                              Map<String, BrokerMetadata> brokers, // <broker addr, metadata>
                               Map<String, TopicMetadata> topics) { // <topic name, metadata>
 }
 
